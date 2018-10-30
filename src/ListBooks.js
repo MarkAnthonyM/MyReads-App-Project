f9c0df5274a722ae84761cc1ccb4b4b28f80cad4
Mark Anthony <markanthonymartinez9@gmail.com>
2018-10-29 23:00:19 -0500

    Feat: add currentlyReading element

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react'
+import './App.css'
 
 class ListBooks extends Component {
   render() {
+    console.log(this.props)
     return (
-      
+      <div className='list-books'>
+        <div className='list-books-title'>
+          <h1>MyReads</h1>
+        </div>
+        <div className='list-books-content'>
+          <div>
+            <div className='bookshelf'>
+              <h2 className='bookshelf-title'>Currently Reading</h2>
+              <div className='bookshelf-books'>
+                <ol className='books-grid'>
+                  {this.props.books.map((book) => (
+                    <li key={book.id}>
+                      <div className='book'>
+                        <div className='book-top'>
+                          <div className='book-cover' style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
