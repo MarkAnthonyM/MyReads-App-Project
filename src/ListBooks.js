5032abe32d07b407b96bb60cc94eb57d8fc6bc1b
Mark Anthony <markanthonymartinez9@gmail.com>
2018-10-31 02:33:23 -0500

    feat: implement SearchBooks component logic

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -20,7 +20,7 @@ class ListBooks extends Component {
                   <div className='book-top'>
                     <div
                       className='book-cover'
-                      style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}>
+                      style={{ width: 128, height: 193, backgroundImage: `url(${(book.imageLinks && book.imageLinks.thumbnail) || ''})`}}>
                     </div>
                     <div className="book-shelf-changer">
                       <select>
